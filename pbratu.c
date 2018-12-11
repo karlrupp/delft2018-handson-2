1f8a7c0001bf59fd2745611800ef4d6cf3b12e7d
Karl Rupp <me@karlrupp.net>
2018-12-11 16:30:16 +0100

    Step 5: Populated with code.

--- pbratu.c
+++ pbratu.c
@@ -30,8 +30,8 @@ The command line options include:\n\
 
     Program usage:  mpiexec -n <procs> ./pbratu [-help] [all PETSc options]
      e.g.,
-      ./pbratu -snes_fd
-      mpiexec -n 2 ./pbratu -snes_fd -log_summary
+      ./pbratu -fd_jacobian -mat_fd_coloring_view_draw -draw_pause -1
+      mpiexec -n 2 ./pbratu -fd_jacobian_ghosted -log_summary
 
   ------------------------------------------------------------------------- */
 
@@ -57,6 +57,7 @@ typedef struct {
   PetscReal lambda;         /* Bratu parameter */
   PetscReal p;              /* Exponent in p-Laplacian */
   PetscReal epsilon;        /* Regularization */
+  PetscInt  jtype;          /* What type of Jacobian to assemble */
 } AppCtx;
 
 /*
@@ -64,6 +65,7 @@ typedef struct {
 */
 static PetscErrorCode FormInitialGuess(DM,Vec);
 static PetscErrorCode FormFunctionLocal(DMDALocalInfo*,PetscScalar**,PetscScalar**,AppCtx*);
+static PetscErrorCode FormJacobianLocal(DMDALocalInfo *info,PetscScalar **x,Mat J,Mat B,AppCtx *user);
 
 #undef __FUNCT__
 #define __FUNCT__ "main"
@@ -76,6 +78,7 @@ int main(int argc,char **argv)
   PetscInt               its;                  /* iterations for convergence */
   SNESConvergedReason    reason;               /* Check convergence */
   PetscReal              bratu_lambda_max = 6.81,bratu_lambda_min = 0.;
+  PetscBool              myJ;
   PetscErrorCode         ierr;
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -87,12 +90,14 @@ int main(int argc,char **argv)
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Initialize problem parameters
   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  user.lambda = 6.0; user.p = 2.0; user.epsilon = 1e-5;
+  user.lambda = 6.0; user.p = 2.0; user.epsilon = 1e-5; user.jtype = 2; myJ = PETSC_FALSE;
   ierr = PetscOptionsBegin(PETSC_COMM_WORLD,NULL,"p-Bratu options",__FILE__);CHKERRQ(ierr);
   {
     ierr = PetscOptionsReal("-lambda","Bratu parameter","",user.lambda,&user.lambda,NULL);CHKERRQ(ierr);
     ierr = PetscOptionsReal("-p","Exponent `p' in p-Laplacian","",user.p,&user.p,NULL);CHKERRQ(ierr);
     ierr = PetscOptionsReal("-epsilon","Strain-regularization in p-Laplacian","",user.epsilon,&user.epsilon,NULL);CHKERRQ(ierr);
+    ierr = PetscOptionsInt("-jtype","Jacobian type, 1=plain, 2=first term","",user.jtype,&user.jtype,NULL);CHKERRQ(ierr);
+    ierr = PetscOptionsBool("-myJ","Provide my Jacobian","",myJ,&myJ,NULL);CHKERRQ(ierr);
   }
   ierr = PetscOptionsEnd();CHKERRQ(ierr);
   if (user.lambda > bratu_lambda_max || user.lambda < bratu_lambda_min) {
@@ -127,7 +132,8 @@ int main(int argc,char **argv)
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Set local residual evaluation routine
   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  ierr = DMDASNESSetFunctionLocal(dm,INSERT_VALUES,(DMDASNESFunction)FormFunctionLocal,&user);CHKERRQ(ierr);
+  ierr = DMDASNESSetFunctionLocal(dm,INSERT_VALUES,(DMDASNESFunction)FormFunctionLocal,(void *)&user);CHKERRQ(ierr);
+  if (myJ) { ierr = DMDASNESSetJacobianLocal(dm,(PetscErrorCode (*)(DMDALocalInfo*,void*,Mat,Mat,void*))FormJacobianLocal,(void *)&user);CHKERRQ(ierr); }
   ierr = DMSetApplicationContext(dm,&user);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -233,9 +239,7 @@ static PetscErrorCode FormInitialGuess(DM dm,Vec X)
 
 /* p-Laplacian diffusivity */
 static inline PetscScalar eta(const AppCtx *ctx,PetscScalar ux,PetscScalar uy)
-{
-  return pow(PetscSqr(ctx->epsilon)+0.5*(ux*ux + uy*uy),0.5*(ctx->p-2.0));
-}
+{return pow(PetscSqr(ctx->epsilon)+0.5*(ux*ux + uy*uy),0.5*(ctx->p-2.));}
 
 /* ------------------------------------------------------------------- */
 #undef __FUNCT__
@@ -288,3 +292,99 @@ static PetscErrorCode FormFunctionLocal(DMDALocalInfo *info,PetscScalar **x,Pets
   }
   PetscFunctionReturn(0);
 }
+
+#undef __FUNCT__
+#define __FUNCT__ "FormJacobianLocal"
+/*
+   FormJacobianLocal - Evaluates Jacobian matrix.
+*/
+static PetscErrorCode FormJacobianLocal(DMDALocalInfo *info,PetscScalar **x,Mat J,Mat B,AppCtx *user)
+{
+  PetscErrorCode ierr;
+  PetscInt       i,j;
+  MatStencil     col[9],row;
+  PetscScalar    v[9];
+  PetscReal      hx,hy,dhx,dhy,hxdhy,hydhx,sc;
+
+  PetscFunctionBegin;
+  hx     = 1.0/(PetscReal)(info->mx-1);
+  hy     = 1.0/(PetscReal)(info->my-1);
+  sc     = hx*hy*user->lambda;
+  dhx    = 1./hx;
+  dhy    = 1./hy;
+  hxdhy  = hx/hy;
+  hydhx  = hy/hx;
+
+  /*
+     Compute entries for the locally owned part of the Jacobian.
+      - PETSc parallel matrix formats are partitioned by
+        contiguous chunks of rows across the processors.
+      - Each processor needs to insert only elements that it owns
+        locally (but any non-local elements will be sent to the
+        appropriate processor during matrix assembly).
+      - Here, we set all entries for a particular row at once.
+  */
+  for (j=info->ys; j<info->ys+info->ym; j++) {
+    for (i=info->xs; i<info->xs+info->xm; i++) {
+      row.j = j; row.i = i;
+      /* boundary points */
+      if (i == 0 || j == 0 || i == info->mx-1 || j == info->my-1) {
+        v[0] = 1.0;
+        ierr = MatSetValuesStencil(B,1,&row,1,&row,v,INSERT_VALUES);CHKERRQ(ierr);
+      } else {
+      /* interior grid points */
+        const PetscScalar
+          ux_E = dhx*(x[j][i+1]-x[j][i]),
+          uy_E = 0.25*dhy*(x[j+1][i]+x[j+1][i+1]-x[j-1][i]-x[j-1][i+1]),
+          ux_W = dhx*(x[j][i]-x[j][i-1]),
+          uy_W = 0.25*dhy*(x[j+1][i-1]+x[j+1][i]-x[j-1][i-1]-x[j-1][i]),
+          ux_N = 0.25*dhx*(x[j][i+1]+x[j+1][i+1]-x[j][i-1]-x[j+1][i-1]),
+          uy_N = dhy*(x[j+1][i]-x[j][i]),
+          ux_S = 0.25*dhx*(x[j-1][i+1]+x[j][i+1]-x[j-1][i-1]-x[j][i-1]),
+          uy_S = dhy*(x[j][i]-x[j-1][i]),
+          u = x[j][i],
+          e_E = eta(user,ux_E,uy_E),
+          e_W = eta(user,ux_W,uy_W),
+          e_N = eta(user,ux_N,uy_N),
+          e_S = eta(user,ux_S,uy_S);
+      /* interior grid points */
+        switch (user->jtype) {
+          case 1:
+            /* Jacobian from p=2 */
+            v[0] = -hxdhy;                                           col[0].j = j-1;   col[0].i = i;
+            v[1] = -hydhx;                                           col[1].j = j;     col[1].i = i-1;
+            v[2] = 2.0*(hydhx + hxdhy) - sc*PetscExpScalar(u);       col[2].j = row.j; col[2].i = row.i;
+            v[3] = -hydhx;                                           col[3].j = j;     col[3].i = i+1;
+            v[4] = -hxdhy;                                           col[4].j = j+1;   col[4].i = i;
+            ierr = MatSetValuesStencil(B,1,&row,5,col,v,INSERT_VALUES);CHKERRQ(ierr);
+            break;
+          case 2:
+            /* Jacobian arising from Picard linearization */
+            v[0] = -hxdhy*e_S;                                           col[0].j = j-1;   col[0].i = i;
+            v[1] = -hydhx*e_W;                                           col[1].j = j;     col[1].i = i-1;
+            v[2] = (e_W+e_E)*hydhx + (e_S+e_N)*hxdhy - sc*PetscExpScalar(u); col[2].j = row.j; col[2].i = row.i;
+            v[3] = -hydhx*e_E;                                           col[3].j = j;     col[3].i = i+1;
+            v[4] = -hxdhy*e_N;                                           col[4].j = j+1;   col[4].i = i;
+            ierr = MatSetValuesStencil(B,1,&row,5,col,v,INSERT_VALUES);CHKERRQ(ierr);
+            break;
+          default:
+            SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Jacobian type %d not implemented",user->jtype);
+        }
+      }
+    }
+  }
+
+  /*
+     Assemble matrix, using the 2-step process:
+       MatAssemblyBegin(), MatAssemblyEnd().
+  */
+  ierr = MatAssemblyBegin(B,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(B,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  /*
+     Tell the matrix we will never add a new nonzero location to the
+     matrix. If we do, it will generate an error.
+  */
+  ierr = MatSetOption(B,MAT_NEW_NONZERO_LOCATION_ERR,PETSC_TRUE);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
